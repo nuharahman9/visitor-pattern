be2abe130cc9b4b0974f15f9e3a43d5d82f7e9a4
vmeno0020 <vishalmenon20@gmail.com>
2021-05-24 22:15:10 -0700

    added setprecision

--- op.hpp
+++ op.hpp
@@ -1,28 +1,42 @@
 #ifndef __OP_HPP__
 #define __OP_HPP__
-
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip> 
 #include "base.hpp"
-#include "visitor.hpp"
 
-class Op : public Base {
-    public:
-        Op(double value) : Base() { 
-		val = value;
-		num = 0; 
-	}
-        virtual double evaluate() { return val; }
-        virtual std::string stringify() { return std::to_string(val); }
-	virtual int number_of_children() {
-		return num; 
-	}
-	virtual Base* get_child(int i) { return nullptr; }
-	virtual void accept(Visitor* visitor, int index){
-		visitor->visit_op(this);
-	}
+using namespace std;
 
-    private:
-        double val;
-	int num;
-};
+class Op: public Base {
+public: 
+  Op(double val) : Base() { 
+    value = val;  
+  }
+  virtual double evaluate() { 
+    return value; 
+  }
+  virtual string stringify() { 
+    stringstream out; 
+    out << setprecision(3) << value;          
+    string output = out.str(); 
+    return output;
+  }
+   virtual int number_of_children() { 
+     return 0; 
+   }
+   virtual Base* get_child(int i) { 
+	return nullptr; 
+}    
+      void accept(Visitor* visitor, int index) { 
+	if (index == 0) { 
+	  visitor->visit_op(this); 
+	}
+	else { cout << "Invalid input" << endl; }
+   }
 
-#endif
+
+private: 
+    double value;   
+  }; 
+ #endif
